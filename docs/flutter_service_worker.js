89e5257ee09c8f7f9947046eb5bb82dfa8838a26
jbayraktar <j.bayraktar@live.de>
2025-05-29 18:43:28 +0200

    Redeploy nach Änderungen

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "11c6aad4c9670730159478c57824de38",
+const RESOURCES = {"flutter_bootstrap.js": "52b2c58de6df1ac3a4807207d4352405",
 "version.json": "63af665b1f5d4eee656d4cd413ad8e79",
 "index.html": "124e42233b5f5807f503a942150848d8",
 "/": "124e42233b5f5807f503a942150848d8",
-"main.dart.js": "1c5feb974ec181d49f39316272bbbb4e",
+"main.dart.js": "4c151058483da743c0d69d107549c9cd",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
